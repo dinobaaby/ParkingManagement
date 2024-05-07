8a1a5000362ccb7f3db18adcd49afd67cd4a78d7
dinobaaby <trinhhieu758@gmail.com>
2024-05-07 12:56:32 +0700

    AddHeaderUI

--- client/parkingmanagement/src/layout/DefaultLayout/Header/index.jsx
+++ client/parkingmanagement/src/layout/DefaultLayout/Header/index.jsx
@@ -2,17 +2,59 @@ import PropTypes from "prop-types";
 import { IoMdMenu } from "react-icons/io";
 import "./Header.css";
 import AccountAction from "../../../components/AccountAction/AccountAction";
+import { Search } from "@mui/icons-material";
+import { useState } from "react";
 
 function Header({ handleMenuClick, style }) {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const handleOpenSearch = () => {
+    setIsSearchOpen(true);
+  };
   return (
-    <header className={`header ${style}`}>
-      <button onClick={handleMenuClick} className="header-menu">
-        <IoMdMenu />
-      </button>
-      <div className="header-account-actions">
-        <AccountAction />
-      </div>
-    </header>
+    <>
+      {isSearchOpen ? (
+        <>
+          <div className={`search-container`}>
+            <div className="search-container-input">
+              <Search />
+              <input placeholder="Search..." />
+            </div>
+            <button>Search</button>
+          </div>
+          <header className={`header ${style}`}>
+            <div className="header-left-action">
+              <button onClick={handleMenuClick} className="header-menu">
+                <IoMdMenu />
+              </button>
+              <button
+                onClick={handleOpenSearch}
+                className="header-search-button"
+              >
+                <Search />
+              </button>
+            </div>
+            <div className="header-account-actions">
+              <AccountAction />
+            </div>
+          </header>
+        </>
+      ) : (
+        <header className={`header ${style}`}>
+          <div className="header-left-action">
+            <button onClick={handleMenuClick} className="header-menu">
+              <IoMdMenu />
+            </button>
+            <button onClick={handleOpenSearch} className="header-search-button">
+              <Search />
+            </button>
+          </div>
+          <div className="header-account-actions">
+            <AccountAction />
+          </div>
+        </header>
+      )}
+    </>
   );
 }
 
